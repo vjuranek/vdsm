8a416a94e9efe64416f5b8657e14d0a08599244d
Vojtech Juranek <vjuranek@redhat.com>
2020-09-22 15:19:36 +0200

    storage: cleanup DM mappings during VG deactivation
    It can happen (e.g.when the storage is not available), that the DM
    mappings for LVs are not removed by LVM (see [1]), and after block SD
    teardown there are still some stale DM links. Devices can be later use
    for another storage and stale links pointing to these devices can lead
    data corruption.
    
    Clean DM mappings manually upon VG deactivation to make sure there are
    not stale DM mappings.
    
    [1] https://bugzilla.redhat.com/1881468
    
    Change-Id: I28cd0802a602cf98a6592b4aac56176fdd388d75
    Bug-Url: https://bugzilla.redhat.com/1863058
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- lib/vdsm/storage/lvm.py
+++ lib/vdsm/storage/lvm.py
@@ -1481,10 +1481,13 @@ def deactivateVG(vgName):
     rc, out, err = _lvminfo.cmd(cmd, _lvminfo._getVGDevs([vgName]))
     _lvminfo._invalidatelvs(vgName)
     if rc != 0:
-        # During deactivation we ignore error here because we don't care about
-        # this vg anymore.
-        log.info("deactivate vg %s failed: rc %s - %s %s (ignored)" %
+        log.info("Error deactivating VG %s: rc=%s out=%s err=%s" %
                  (vgName, rc, out, err))
+
+        # When the storage is not available, DM mappings for LVs are not
+        # removed by LVM, so we have to clean it up manually. For more details
+        # see https://bugzilla.redhat.com/1881468
+        removeVgMapping(vgName)
 
 
 def invalidateVG(vgName, invalidateLVs=True, invalidatePVs=False):
